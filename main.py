aa934a2ea60ea3c44fedbf192ac1dcc002a7a595
AxizY <24615015+AxizY@users.noreply.github.com>
2020-02-23 10:08:50 -0500

    Template

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+f1 =  [
+""
+""
+""
+""
+]
+f2 =  [
+""
+""
+""
+""
+]
+f3 =  [
+""
+""
+""
+""
+]
+f4 =  [
+""
+""
+""
+""
+]
+f5 =  [
+""
+""
+""
+""
+]
+
+def play(fps):
+  print(f1)
+  time.sleep(fps)
+  print(f2)
+  time.sleep(fps)
+  print(f3)
+  time.sleep(fps)
+  print(f4)
+  time.sleep(fps)
+  print(f5)
