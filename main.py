b00d132890d731210697cdbe5140c7a5a08de590
AxizY <24615015+AxizY@users.noreply.github.com>
2020-02-23 10:16:28 -0500

    Example

--- main.py
+++ main.py
@@ -1,42 +1,56 @@
+from os import system, name 
 import time
 f1 =  [
-""
-""
-""
-""
+"#",
+" ",
+" ",
+" "
 ]
 f2 =  [
-""
-""
-""
-""
+" ",
+"#",
+" ",
+" "
 ]
 f3 =  [
-""
-""
-""
-""
+" ",
+" ",
+"#",
+" "
 ]
 f4 =  [
-""
-""
-""
-""
+" ",
+" ",
+" ",
+"#"
 ]
 f5 =  [
-""
-""
-""
-""
+" ",
+" ",
+" ",
+"#"
 ]
 
 def play(fps):
   print(f1)
   time.sleep(fps)
+  clear()
   print(f2)
   time.sleep(fps)
+  clear()
   print(f3)
   time.sleep(fps)
+  clear()
   print(f4)
   time.sleep(fps)
+  clear()
   print(f5)
+  time.sleep(fps)
+  clear()
+  
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+    else: 
+        _ = system('clear') 
+play(.3)
