d0531b737d19413bf0f8f3329bf9e2ac382490a9
AxizY <24615015+AxizY@users.noreply.github.com>
2020-04-30 21:09:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,13 @@
 from os import system, name 
 import time
-f1 =  [
-"#",
-" ",
-" ",
-" "
-]
-f2 =  [
-" ",
-"#",
-" ",
-" "
-]
-f3 =  [
-" ",
-" ",
-"#",
-" "
-]
-f4 =  [
-" ",
-" ",
-" ",
-"#"
-]
-f5 =  [
-" ",
-" ",
-" ",
-"#"
-]
-def printa(a):
-  for b in a:
-    print(b)
+anim=[]
 def play(fps):
-  printa(f1)
-  time.sleep(fps)
-  clear()
-  printa(f2)
-  time.sleep(fps)
-  clear()
-  printa(f3)
-  time.sleep(fps)
-  clear()
-  printa(f4)
-  time.sleep(fps)
-  clear()
-  printa(f5)
-  time.sleep(fps)
-  clear()
-  
-def clear(): 
-    if name == 'nt': 
-        _ = system('cls') 
-    else: 
-        _ = system('clear') 
-play(.3)
+    timer = fps
+    for c in range(len(anim)):
+        for b in anim[c]:
+            if isinstance(b, float) == False: print(b)
+            else: timer = b
+        time.sleep(timer)
+        if name == 'nt': _ = system('cls') 
+        else: _ = system('clear')
+        timer = fps
