cc40736fe432860ee1b7a18e1e38756dc74cd7a5
AxizY <24615015+AxizY@users.noreply.github.com>
2020-04-30 21:10:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from os import system, name
 import time
 anim=[]
 def play(fps):
-    timer = fps
-    for c in range(len(anim)):
-        for b in anim[c]:
-            if isinstance(b, float) == False: print(b)
-            else: timer = b
-        time.sleep(timer)
-        if name == 'nt': _ = system('cls') 
-        else: _ = system('clear')
-        timer = fps
+ timer = fps
+ for c in range(len(anim)):
+  for b in anim[c]:
+   if isinstance(b, float) == False: print(b)
+   else: timer = b
+  time.sleep(timer)
+  if name == 'nt': _ = system('cls') 
+  else: _ = system('clear')
+  timer = fps
