95e940ce174f543c2e4dbe4f7cc70dc0267ec4fe
AxizY <24615015+AxizY@users.noreply.github.com>
2020-05-16 22:27:04 -0400

    Create rpsbot.py

---
+++ rpsbot.py
@@ -0,0 +1,1 @@
+import random;moves=['rock','paper','scissors'];bw=0;pw=0;a=0;b=random.randint(0,2);a=input('move?: ');ab=0 if a=="rock" else 1 if a!="scissors" else 2;c=(3+ab-b)%3;print('Bot Move:{}'.format(moves[b]));print("win" if c==1 else "lose" if c!=0 else "tie")
