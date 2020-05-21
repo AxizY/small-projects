c84f4e05b753f9da8deb125ee23beaafae8beeff
AxizY <24615015+AxizY@users.noreply.github.com>
2020-05-21 14:44:31 -0400

    Create image-compress.py

---
+++ image-compress.py
@@ -0,0 +1,8 @@
+import cv2
+import math
+def compress(num,n):return(math.ceil(num/n))
+values = []
+img = cv2.imread('photo', 0)
+x=0;y=0;a=0;k=0
+for y in range(0, img.shape[0]):for x in range(0, img.shape[1]):values.append(compress(img[y,x],img.shape[0])) 
+for a in range(0,compress(len(values),img.shape[1])):k = a-1;print(values[img.shape[1]*k:a*img.shape[1]])
