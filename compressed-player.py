589c2faccb3d0f3f90f293059d58827be3a51558
AxizY <24615015+AxizY@users.noreply.github.com>
2020-11-05 19:40:59 -0500

    Update compressed-player.py

--- compressed-player.py
+++ compressed-player.py
@@ -1,1 +1,10 @@
-
+import time
+time.sleep(3)
+f = open("pixeldata.txt", "r")
+for x in f:
+    for o in x:
+        if o != "\n":
+            playsound(f'{o}.wav')
+            time.sleep(.75)
+        else: time.sleep(1)
+f.close()
