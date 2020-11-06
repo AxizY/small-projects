8ac8db720eb82a5f0dc8d0d938866af8ae0daafe
AxizY <24615015+AxizY@users.noreply.github.com>
2020-11-05 19:41:24 -0500

    for player

--- image-compress.py
+++ image-compress.py
@@ -7,12 +7,12 @@ f=open("pixeldata.txt", "a+")
 img = cv2.imread(str(sys.argv[1]).encode('unicode-escape').decode(), 0) # specifies image location on computer - converts image to grayscale
 for y in range(0, img.shape[0]): # for every y
   for x in range(0, img.shape[1]): # for every x in that y
-    values+="  "+str(math.ceil(img[y,x]/int(sys.argv[2]))) # adds (the pixel grayscale value divided by the y is then rounded up) to the string
+    values+=str(math.floor(img[y,x]/int(sys.argv[2]))) # adds (the pixel grayscale value divided by the y is then rounded up) to the string
   values+="\n" # adds a new line to the string
 open("pixeldata.txt", 'w').close() # clears file content
 f.write(values) # writes the new values
 print("Values have been appended/replaced!") # prints out the results/ the string
-os.system("open pixeldata.txt") # opens textedit may not work on windows
+os.system("open pixeldata.txt") # opens textedit, may not work on windows
 # to run do
 # python3 scriptlocation imagelocation 120
 # in terminal ex.
